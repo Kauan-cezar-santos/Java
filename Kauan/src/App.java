f239060f5545595d6b5c3ca6a74f83b90c2cd17f
Kauan-cezar-santos <165854176+Kauan-cezar-santos@users.noreply.github.com>
2024-04-03 21:56:45 -0300

    Update App.java

--- Kauan/src/App.java
+++ Kauan/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    int numero_inteiro = 10;
+    int numerointeiro = 10;
+    int numinteiro = 10;
+
+        System.out.println(numero_inteiro);
+        System.out.println("O valor é: " + numero_inteiro);
+        System.out.println("valor final: " + numerointeiro);
+
+  
     }
 }
